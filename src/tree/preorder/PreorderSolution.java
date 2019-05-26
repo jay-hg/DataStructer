12141859291b21b9abb42aa419791f894c6df44a
jay-hg <1175867101@qq.com>
2019-05-26 17:35:48 +0800

    先序遍历

---
+++ src/tree/preorder/PreorderSolution.java
@@ -0,0 +1,17 @@
+package tree.preorder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreorderSolution {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root == null) return list;
+        list.add(root.val);
+
+        preorderTraversal(root.left);
+
+        preorderTraversal(root.right);
+        return list;
+    }
+}
---
+++ src/tree/preorder/PreorderSolutionUseStack.java
@@ -0,0 +1,31 @@
+package tree.preorder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class PreorderSolutionUseStack {
+    List<Integer> list = new ArrayList<>();
+    Stack<TreeNode> stack = new Stack<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        while (true) {
+            vistAlongLeftBrunch(root);
+            if (stack.isEmpty()) {
+                break;
+            }
+            root = stack.pop();
+        }
+        return list;
+    }
+
+    private void vistAlongLeftBrunch(TreeNode treeNode) {
+        while (treeNode != null) {
+            list.add(treeNode.val);
+            if (treeNode.right != null) {
+                stack.push(treeNode.right);
+            }
+            treeNode = treeNode.left;
+        }
+    }
+}
