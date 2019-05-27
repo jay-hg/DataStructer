7dbd17bad2f44783278f37eb76ba8df5f028d700
caihg <1175867101@qq.com>
2019-05-27 17:55:12 +0800

    二叉树后序遍历 一

---
+++ src/tree/postorder/PostorderSolution.java
@@ -0,0 +1,17 @@
+package tree.postorder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostorderSolution {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        list.add(root.val);
+        return list;
+    }
+}
---
+++ src/tree/postorder/PostorderSolutionUseStack.java
@@ -0,0 +1,12 @@
+package tree.postorder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostorderSolutionUseStack {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+
+        return list;
+    }
+}
