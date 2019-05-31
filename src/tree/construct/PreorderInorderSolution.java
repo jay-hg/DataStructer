d921501a87b776cea30e66aaa0983c0e06d23de0
jay-hg <1175867101@qq.com>
2019-06-01 00:06:08 +0800

    前序和中序构建二叉树

---
+++ src/tree/construct/PreorderInorderSolution.java
@@ -0,0 +1,40 @@
+package tree.construct;
+
+public class PreorderInorderSolution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return constructTree(preorder, 0, preorder.length, inorder, 0, inorder.length);
+    }
+
+    private TreeNode constructTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
+        if (preStart == preEnd) {
+            return null;
+        }
+        //1.找到跟节点
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int rootIdx = findRootIdx(inorder, inStart, inEnd, root.val); //找到跟节点在inorder的位置
+        int inorderLeft = inStart;
+        int inorderLeftEnd = rootIdx;
+        int inorderRight = rootIdx+1;
+        int inorderRightEnd = inEnd;
+        int preorderLeft = preStart+1;
+        int preorderLeftEnd = preStart+1+rootIdx-inStart;
+        int preorderRight = preorderLeftEnd;
+        int preorderRightEnd = preEnd;
+
+        //2.递归构建左子树
+        root.left = constructTree(preorder, preorderLeft, preorderLeftEnd, inorder, inorderLeft, inorderLeftEnd);
+        //3.递归构建右子树
+        root.right = constructTree(preorder, preorderRight, preorderRightEnd, inorder, inorderRight, inorderRightEnd);
+        return root;
+    }
+
+    private int findRootIdx(int[] inorder, int inStart, int inEnd, int val) {
+        while (inStart < inEnd) {
+            if (val == inorder[inStart]) {
+                return inStart;
+            }
+            inStart++;
+        }
+        return inEnd;
+    }
+}
---
+++ src/tree/construct/TreeNode.java
@@ -0,0 +1,8 @@
+package tree.construct;
+
+public class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int val;
+    TreeNode(int x) { val = x; }
+}
