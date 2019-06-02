9c5469c0b0b1d72290294180049e0595f9287157
jay-hg <1175867101@qq.com>
2019-06-02 14:31:25 +0800

    对称二叉树（bfs实现）

---
+++ src/tree/SymmetricTree.java
@@ -0,0 +1,41 @@
+package tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            if (t1.val != t2.val) {
+                return false;
+            }
+            queue.offer(t1.left);
+            queue.offer(t2.right);
+            queue.offer(t1.right);
+            queue.offer(t2.left);
+        }
+        return true;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
